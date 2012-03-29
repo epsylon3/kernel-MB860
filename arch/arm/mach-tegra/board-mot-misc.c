343689bea54aa77c98de99c3cb9f4f961c82d17c
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-03-29 11:26:39 +0200

    board-mot-misc: indent cleanup
    Change-Id: If46af4c00d856e7c8b3b86255a7e7fce27e11913

--- arch/arm/mach-tegra/board-mot-misc.c
+++ arch/arm/mach-tegra/board-mot-misc.c
@@ -35,61 +35,56 @@
 #ifdef CONFIG_APANIC_RAM
 /* The kernel modifies these, so we need two. */
 static struct resource apanic_handle_ram_resource[] = {
-    {
-        .flags = IORESOURCE_MEM,
-    },
+	{
+		.flags = IORESOURCE_MEM,
+	},
 };
 static struct resource apanic_report_ram_resource[] = {
-    {
-        .flags = IORESOURCE_MEM,
-    },
+	{
+		.flags = IORESOURCE_MEM,
+	},
 };
 static struct platform_device apanic_handle_ram_platform_device = {
-    .name          = "apanic_handle_ram",
-    .id            = 0,
-    .num_resources = ARRAY_SIZE(apanic_handle_ram_resource),
-    .resource      = apanic_handle_ram_resource,
-
+	.name          = "apanic_handle_ram",
+	.id            = 0,
+	.num_resources = ARRAY_SIZE(apanic_handle_ram_resource),
+	.resource      = apanic_handle_ram_resource,
 };
 static struct platform_device apanic_report_ram_platform_device = {
-    .name          = "apanic_report_ram",
-    .id            = 0,
-    .num_resources = ARRAY_SIZE(apanic_report_ram_resource),
-    .resource      = apanic_report_ram_resource,
-
+	.name          = "apanic_report_ram",
+	.id            = 0,
+	.num_resources = ARRAY_SIZE(apanic_report_ram_resource),
+	.resource      = apanic_report_ram_resource,
 };
 
 static int __init apanic_ram_init(void)
 {
-    int result = -ENOMEM;
+	int result = -ENOMEM;
 
 	printk(KERN_INFO "apanic_ram_init\n");
-    if (mot_get_apanic_resource(&apanic_handle_ram_resource[0]) == 0)
-    {
-    	result = platform_device_register(&apanic_handle_ram_platform_device);
-    }
-    if (result == 0 &&
-        mot_get_apanic_resource(&apanic_report_ram_resource[0]) == 0)
-    {
-        result = platform_device_register(&apanic_report_ram_platform_device);
-    }
+	if (mot_get_apanic_resource(&apanic_handle_ram_resource[0]) == 0) {
+		result = platform_device_register(&apanic_handle_ram_platform_device);
+	}
+	if (result == 0 &&
+	    mot_get_apanic_resource(&apanic_report_ram_resource[0]) == 0) {
+		result = platform_device_register(&apanic_report_ram_platform_device);
+	}
 
-    return result;
+	return result;
 }
 #endif
 
-#ifdef CONFIG_APANIC_MMC
 
+#ifdef CONFIG_APANIC_MMC
 static struct tegra_sdhci_simple_platform_data tegra_sdhci_simple_platform_data;
 static struct platform_device tegra_sdhci_simple_device;
 
 static struct apanic_mmc_platform_data apanic_mmc_platform_data;
 
 static struct platform_device apanic_handle_mmc_platform_device = {
-	.name          = "apanic_handle_mmc",
-	.id            = 0,
-	.dev =
-	{
+	.name	= "apanic_handle_mmc",
+	.id	= 0,
+	.dev = {
 		.platform_data = &apanic_mmc_platform_data,
 	}
 };
@@ -155,11 +150,12 @@ int apanic_mmc_init(void)
 void mot_set_hsj_mux(short hsj_mux_gpio)
 {
 	/* set pin M 2 to 1 to route audio onto headset or 0 to route console uart */
-
 	if( gpio_request(TEGRA_GPIO_PM2, "hs_detect_enable") < 0 )
-  		printk("\n%s: gpio_request error gpio %d  \n", 
+		printk("\n%s: gpio_request error gpio %d  \n",
 				__FUNCTION__,TEGRA_GPIO_PM2);
 	else {
+		pr_info("%s: GPIO PM2 config set to %s\n", __func__,
+			hsj_mux_gpio ? "audio":"uart");
 		gpio_direction_output(TEGRA_GPIO_PM2, hsj_mux_gpio);
 	}
 }
@@ -169,50 +165,41 @@ void mot_set_hsj_mux(short hsj_mux_gpio)
  */
 static struct sec_platform_data mot_sec_platform_data;
 static struct platform_device mot_sec_platform_device = {
-    .name          = "sec",
-    .id            = 0,
-    .dev =
-    {
-        .platform_data = &mot_sec_platform_data,
-    }
+	.name	= "sec",
+	.id	= 0,
+	.dev	= {
+		.platform_data = &mot_sec_platform_data,
+	}
 };
 
 void mot_sec_init(void)
 {
-    	platform_device_register(&mot_sec_platform_device);
+	platform_device_register(&mot_sec_platform_device);
 }
-
 
 /*
  * TCMD
  */
 static struct tcmd_driver_platform_data mot_tcmd_platform_data;
 static struct platform_device mot_tcmd_platform_device = {
-    .name          = "tcmd_driver",
-    .id            = 0,
-    .dev = {
+	.name	= "tcmd_driver",
+	.id	= 0,
+	.dev	= {
 		.platform_data = &mot_tcmd_platform_data,
-    }
+	}
 };
 
 void mot_tcmd_init(void)
 {
-	mot_tcmd_platform_data.gpio_list[TCMD_GPIO_ISL29030_INT]
-      = TEGRA_GPIO_PE1;
-	mot_tcmd_platform_data.gpio_list[TCMD_GPIO_KXTF9_INT]
-      = TEGRA_GPIO_PV3;
-	mot_tcmd_platform_data.gpio_list[TCMD_GPIO_MMC_DETECT]
-      = TEGRA_GPIO_PI5;
-	mot_tcmd_platform_data.gpio_list[TCMD_GPIO_INT_MAX_NUM]
-      = -1;
+	mot_tcmd_platform_data.gpio_list[TCMD_GPIO_ISL29030_INT] = TEGRA_GPIO_PE1;
+	mot_tcmd_platform_data.gpio_list[TCMD_GPIO_KXTF9_INT]    = TEGRA_GPIO_PV3;
+	mot_tcmd_platform_data.gpio_list[TCMD_GPIO_MMC_DETECT]   = TEGRA_GPIO_PI5;
+	mot_tcmd_platform_data.gpio_list[TCMD_GPIO_INT_MAX_NUM]  = -1;
 	mot_tcmd_platform_data.size = TCMD_GPIO_INT_MAX_NUM;
 
-	if (machine_is_etna()) {
-		if (system_rev == 0x1100) {
-			mot_tcmd_platform_data.gpio_list[TCMD_GPIO_KXTF9_INT]
-				= TEGRA_GPIO_PN4;
-      }
-    }
+	if (machine_is_etna() && system_rev == 0x1100) {
+		mot_tcmd_platform_data.gpio_list[TCMD_GPIO_KXTF9_INT] = TEGRA_GPIO_PN4;
+	}
 
 	platform_device_register(&mot_tcmd_platform_device);
 }
@@ -232,63 +219,62 @@ unsigned char lpddr2_mr[12];
 
 int MotorolaBootFBArgGet(unsigned int *arg)
 {
-    *arg = s_MotorolaFBInfo;
-    return 0;
+	*arg = s_MotorolaFBInfo;
+	return 0;
 }
 
 int MotorolaBootDispArgGet(unsigned int *arg)
 {
-    if(s_MotorolaDispInfo)
-    {
-        *arg = s_MotorolaDispInfo;
-        return 0;
-    }
-
-    return -1;
+	if(s_MotorolaDispInfo) {
+		*arg = s_MotorolaDispInfo;
+		return 0;
+	}
+	return -1;
 }
-
 
 /*
  * Parse the Motorola-specific ATAG
  */
 static int __init parse_tag_motorola(const struct tag *tag)
 {
-    const struct tag_motorola *moto_tag = &tag->u.motorola;
-    int i = 0;
+	const struct tag_motorola *moto_tag = &tag->u.motorola;
+//	int i = 0;
 
-    s_MotorolaDispInfo = moto_tag->panel_size;
-    s_MotorolaFBInfo = moto_tag->allow_fb_open;
+	s_MotorolaDispInfo = moto_tag->panel_size;
+	s_MotorolaFBInfo = moto_tag->allow_fb_open;
 
-    #ifdef CONFIG_MOT_ALLOW_FB_OPEN
-    s_MotorolaFBInfo = 1; // allow fb open for charger and bootmenu (minui)
-    #endif
+#ifdef CONFIG_MOT_ALLOW_FB_OPEN
+	s_MotorolaFBInfo = 1; // allow fb open for charger and bootmenu (minui)
+#endif
 
-    mot_sec_platform_data.fl_factory = moto_tag->in_factory;
+	mot_sec_platform_data.fl_factory = moto_tag->in_factory;
 
-    bootloader_ver_major = moto_tag->bl_ver_major;
-    bootloader_ver_minor = moto_tag->bl_ver_minor;
-    uboot_ver_major = moto_tag->uboot_ver_major;
-    uboot_ver_minor = moto_tag->uboot_ver_minor;
-    bi_set_cid_recover_boot(moto_tag->cid_suspend_boot);
+	bootloader_ver_major = moto_tag->bl_ver_major;
+	bootloader_ver_minor = moto_tag->bl_ver_minor;
+	uboot_ver_major = moto_tag->uboot_ver_major;
+	uboot_ver_minor = moto_tag->uboot_ver_minor;
+	bi_set_cid_recover_boot(moto_tag->cid_suspend_boot);
 
-    pr_info("%s: bootloader v%d.%d\n", __func__, bootloader_ver_major, bootloader_ver_minor);
-    pr_info("%s: panel_size: 0x%x\n", __func__, s_MotorolaDispInfo);
-    pr_info("%s: allow_fb_open: %x\n", __func__, s_MotorolaFBInfo);
-    pr_info("%s: factory: %d\n", __func__, mot_sec_platform_data.fl_factory);
-    pr_info("%s: cid_suspend_boot: %u\n", __func__, (unsigned)moto_tag->cid_suspend_boot);
+	pr_info("%s: bootloader v%d.%d\n", __func__, bootloader_ver_major, bootloader_ver_minor);
+	pr_info("%s: panel_size: 0x%x\n", __func__, s_MotorolaDispInfo);
+	pr_info("%s: allow_fb_open: %x\n", __func__, s_MotorolaFBInfo);
+	pr_info("%s: factory: %d\n", __func__, mot_sec_platform_data.fl_factory);
+	pr_info("%s: cid_suspend_boot: %u\n", __func__, (unsigned)moto_tag->cid_suspend_boot);
 
-    /*
-     *	Dump memory information
-     */
-     /* FIXME:  Add eMMC support */
+	/*
+	 * Dump memory information
+	 * FIXME: Add eMMC support (and make it report valid informations :p)
+	 */
+/*
 	for (i = 0; i < 12; i++) {
 		lpddr2_mr[i] = moto_tag->at_lpddr2_mr[i];
 		pr_info("%s: LPDDR2 MR%d:     0x%04X (0x%04X)\n", __func__, i,
 			lpddr2_mr[i],
 			moto_tag->at_lpddr2_mr[i]);
 	}
+*/
 
-    return 0;
+	return 0;
 }
 __tagtable(ATAG_MOTOROLA, parse_tag_motorola);
 
@@ -310,7 +296,7 @@ __tagtable(ATAG_BLDEBUG, parse_tag_bldebug);
  */
 
 static int keymap_update_connect(struct input_handler *handler, struct input_dev *dev,
-					  const struct input_device_id *id)
+                                 const struct input_device_id *id)
  {
 	if (strcmp(dev->name , "tegra-kbc"))  return 0;
 
@@ -325,7 +311,6 @@ static int keymap_update_connect(struct input_handler *handler, struct input_dev
 	set_bit(0xE8, dev->keybit); // DPAD_CENTER
 	set_bit(0xA2, dev->keybit); // Power
 
-
 	pr_info("keymap_update add key codes to device %s\n", dev->name);
 	return 0;
  }
@@ -338,9 +323,8 @@ static const struct input_device_id keymap_update_ids[] = {
 		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
 		.evbit = { BIT_MASK(EV_KEY) },
 	},
-	{ },
+	{},
 };
-
 MODULE_DEVICE_TABLE(input, keymap_update_ids);
 
 static struct input_handler keymap_update_handler = {
@@ -351,8 +335,8 @@ static struct input_handler keymap_update_handler = {
 	.id_table   = keymap_update_ids,
 };
 
-
 void mot_keymap_update_init(void)
 {
 	input_register_handler(&keymap_update_handler);
 }
+
